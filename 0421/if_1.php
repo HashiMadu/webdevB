133f5a75a672172e3a6e4cd37f9b7568fd50b263
Hashini madushika <Hashimadujp92@gmail.com>
2025-04-21 16:21:20 +0900

    quection

--- 0421/if_1.php
+++ 0421/if_1.php
@@ -1,1 +1,29 @@
-#if文またやる
+<?php
+$condition = false;
+if ($condition) {
+    echo '条件はtrueです。';
+}
+?>
+<br>
+<?php
+$a = 1;
+$b = "1";
+if ($a === 1) {
+    echo "aは数値型です。";
+}
+if ($b === "1") {
+    echo "bは文字列型です。";
+}
+if ($a == $b) {
+    echo "aとbは等しい値です。";
+}
+?>
+<?php
+echo "<br>";
+if ($a === $b) {
+    echo "aとbは型も値も等しい値です。";
+} else {
+    echo "aとbは型が等しくありません。";
+} ?>
+
+<br>
